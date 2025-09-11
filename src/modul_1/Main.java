eeec22b8a47bbad3b50924027bbf03e667dd6a4b
Nama Kamu <dwiidarahmadan@gmail.com>
2025-09-11 15:15:22 +0700

    Initial commit

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package modul_1;
+
+public class Main {
+    //Fungsi khusus sebagai enter poin dari aplikasi yang di buat.
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
